778f5c3bfcf25d07862caedd4dc5f54995a4dbef
ogneyar <ogneyar-ne@ya.ru>
2020-11-30 20:31:10 +0300

    sss

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <div id="app">
+  <div id="app"> 
+
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <hr>
+    <router-link to="/">Go to Home</router-link> | 
+    <router-link to="/foo">Go to Foo</router-link> | 
+    <router-link to="/bar">Go to Bar</router-link>
+    <router-view></router-view>
+    <hr>
+
+    <HelloWorld msg="Welcome to Your Vue.js App"/>  
+    
   </div>
 </template>
 
@@ -23,6 +33,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Real Home Page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
