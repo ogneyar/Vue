d30fd3a5541e7fb880b5ecaa8be782a33816d6b8
ogneyar <ogneyar-ne@ya.ru>
2020-12-01 12:33:33 +0300

    ff

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var express = require('express');
 var	port = process.env.PORT || 8000;
 
 express().use(express.static('dist'))
-	.get('*', (req, res) => res.render('dist/index.html'))
+	.get('*', (req, res) => res.sendFile(__dirname + '/dist/index.html'))
 	.listen(port, () => console.log(`Starting server. Listening on ${ port }`));
